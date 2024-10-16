5de2627e79dc65b8be81b9634df32fb289586b39
isabela <isabe@DESKTOP-7JLA0L9>
2024-10-16 14:16:20 -0300

    Adicionar arquivos de projeto.

---
+++ aprendendo c#/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int D = Convert.ToInt32(Console.ReadLine());
+int conta = (A * B - C * D);
+Console.WriteLine("DIFERENCA = " + conta);
+
+
+
